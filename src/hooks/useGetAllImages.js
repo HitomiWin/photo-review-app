38f329c206626465b505be40c053aca281c222e1
HitomiWin <hitomi.winberg@gmail.com>
2022-01-10 16:14:16 +0100

    add condition not to get another owners photos

--- src/hooks/useGetAllImages.js
+++ src/hooks/useGetAllImages.js
@@ -1,14 +1,33 @@
-import { useFirestoreQueryData} from "@react-query-firebase/firestore";
-import { collection, query, orderBy} from "firebase/firestore";
-import { db } from "../firebase";
+import {
+  useFirestoreQueryData
+} from "@react-query-firebase/firestore";
+import {
+  collection,
+  query,
+  orderBy,
+  where
+} from "firebase/firestore";
+import {
+  db
+} from "../firebase";
+import {
+  useAuthContext
+} from "../contexts/AuthContext";
 
-const useGetAllImages = (id, col) => {
+const useGetAllImages = (id, col, preview) => {
+  const {
+    currentUser
+  } = useAuthContext();
   const imagesRef = collection(db, col, id, "images");
   const queryKey = ["images", id];
-  const queryRef = query(
-    imagesRef,
-    orderBy("created", 'desc')
-  );
+  const queryRef = preview ?
+    query(
+      imagesRef,
+      orderBy("created", 'desc')
+    ) : query(
+      imagesRef,
+      where("owner", "==", currentUser.uid),
+      orderBy("created", 'desc'))
 
   const getImagesQuery = useFirestoreQueryData(
     queryKey,
